53f44208a7c8083c9ccfa329b85adf3e1756be61
Ben Walpole <benwalpole95@gmail.com>
2026-06-06 09:53:24 +0100

    using 11ty

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+export default function (eleventyConfig) {
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setOutputDirectory("dist");
+  // Set directories to pass through to the dist folder
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+}
+
+export const config = {
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
+};
