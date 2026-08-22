1cfa0a10b6a927e9bc940d499c6e22553776067c
Ben Walpole <btm.walpole@gmail.com>
2026-08-22 15:00:29 +0100

    now rendering image from sanity

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,20 @@
+import { urlFor } from "./src/_utils/imageUrl.js";
+
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("_site");
   // Set directories to pass through to the dist folder
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/fonts");
+   eleventyConfig.addFilter("sanityImage", (image, width) => {
+    const imageBuilder = urlFor(image);
+
+    if (width) {
+      imageBuilder.width(width);
+    }
+
+    return imageBuilder.url();
+  });
 }
 
 export const config = {
